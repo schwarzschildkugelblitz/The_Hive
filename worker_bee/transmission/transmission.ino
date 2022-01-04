57f1c04dd70b73a69449a5309f8250eb8fc7d825
tanmay1302 <86514579+tanmay1302@users.noreply.github.com>
2022-01-05 00:04:29 +0530

    Add files via upload

--- worker_bee/transmission/transmission.ino
+++ worker_bee/transmission/transmission.ino
@@ -5,15 +5,38 @@
 #include <SPI.h>
 #include <RH_NRF24.h>
 
+#define stopButton  2
 
-// 1st 8 bit number -> 2 bit address + 6 bit differential speed
+// 1st 8 bit number -> 2 bit address + 6 bit modes
 // 2nd 8 bit number => 1 sign bit , 4 bit base speed , 3 empty bits
 RH_NRF24 nrf24(9, 10);
+uint8_t data[2];
+int x =0;
+uint8_t parsedData[2];
+void stopAll() // modify the data to be sent
+{
+
+while (digitalRead(2)==HIGH)
+{
+  for(x = 0; x < 4;x++)
+  {
+ 
+  parsedData[0] = x+4*58; // XXXX(A1)(A0) & 000011 + 1110 1000 = 1110 10(A1)(A0) : 58 is the stop code
+  nrf24.send(parsedData, sizeof(parsedData)); // send data
+  nrf24.waitPacketSent();
+   parsedData[0] = x+4*58; // XXXX(A1)(A0) & 000011 + 1110 1000 = 1110 10(A1)(A0) : 58 is the stop code
+  nrf24.send(parsedData, sizeof(parsedData)); // send data
+  nrf24.waitPacketSent();
+  }
+}
+}
+
 
 void setup()
 {
   Serial.begin(115200);
-
+  pinMode(stopButton, INPUT_PULLUP); // already refrenced to +5v / HIGH via pullup
+  attachInterrupt(digitalPinToInterrupt(stopButton), stopAll, RISING);
   if (!nrf24.init());
 //    Serial.println("init failed");
   if (!nrf24.setChannel(1));
@@ -23,16 +46,17 @@ void setup()
 
 }
 
-float timi=millis(),timf,D,x,V=0,filter_state=0,integrator_state=0;
-float Kp=0.00025,Kd=0.0155,Ki=0.000085,N=34;// tuned values
-int add,speed,sign,speed2=0,misc=0;
-int theta,dist,i,k; // index vars
-uint8_t data[2];
+// float timi=millis(),timf,D,x,V=0,filter_state=0,integrator_state=0; // implemented in py
+// float Kp=0.00025,Kd=0.0155,Ki=0.000085,N=34;// tuned values
+// int add,speed,sign,speed2=0,misc=0;
+ int theta,dist,i,k; // index vars
+
 
 void loop()
 {
   // 0 1 2 3 -> angle +ve , distance +ve , 4 5  
-  uint8_t parsedData[] = {0,0}; // do not change default state 
+  parsedData[0] =0;// do not change default state 
+  parsedData[1] =0;
   String s1;
   char recv[10];//max
   
@@ -55,7 +79,7 @@ void loop()
    
     i++;
   }
-  
+   
   nrf24.send(parsedData, sizeof(parsedData)); // send data
   nrf24.waitPacketSent();
   //delay(22); // maintain sync
